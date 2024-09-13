906795f9d6f6922f91f65f405c43ae7290a116cd
João Victor <offreflix@gmail.com>
2024-09-13 01:08:47 -0300

    feat: seed database

---
+++ server/src/db/seed.ts
@@ -0,0 +1,38 @@
+import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals } from './schema'
+
+async function seed() {
+  await db.delete(goalCompletions)
+  await db.delete(goals)
+
+  const result = await db
+    .insert(goals)
+    .values([
+      {
+        title: 'Learn to code',
+        desiredWeeklyFrequency: 5,
+      },
+      {
+        title: 'Exercise',
+        desiredWeeklyFrequency: 3,
+      },
+      {
+        title: 'Eat healthy',
+        desiredWeeklyFrequency: 7,
+      },
+    ])
+    .returning()
+
+  const startOfWeek = dayjs().startOf('week')
+
+  await db.insert(goalCompletions).values([
+    { goalId: result[0].id, createdAt: startOfWeek.toDate() },
+    { goalId: result[1].id, createdAt: startOfWeek.add(1, 'day').toDate() },
+  ])
+}
+
+seed()
+  .then(() => console.log('Seeded database'))
+  .catch(console.error)
+  .finally(() => client.end())
