6a7e9c858b7693f1526060e35e10644568a3fa10
João Victor <offreflix@gmail.com>
2024-09-14 12:03:10 -0300

    feat: goals route

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,22 @@
 import fastify from 'fastify'
+import { createGoal } from '../services/create-goal'
+import z from 'zod'
 
 const app = fastify()
+
+app.post('/goals', async (request) => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number(),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
+})
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server listening on port 3333')
